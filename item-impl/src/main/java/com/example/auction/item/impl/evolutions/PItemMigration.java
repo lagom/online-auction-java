8c237cd7f5148d1dca6d1e57a7dd4966426123e1
Ignasi Marimon-Clos <ignasi@lightbend.com>
2016-11-30 20:13:20 +0100

    Adds evolution for PItem (extract fields)

---
+++ item-impl/src/main/java/com/example/auction/item/impl/evolutions/PItemMigration.java
@@ -0,0 +1,36 @@
+package com.example.auction.item.impl.evolutions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.lightbend.lagom.serialization.JacksonJsonMigration;
+
+/**
+ *
+ */
+public class PItemMigration extends JacksonJsonMigration {
+    @Override
+    public int currentVersion() {
+        return 2;
+    }
+
+    @Override
+    public JsonNode transform(int fromVersion, JsonNode json) {
+        ObjectNode root = (ObjectNode) json;
+        if (fromVersion <= 1) {
+            ObjectNode itemDetails = root.with("itemDetails");
+            itemDetails.set("title", root.get("title"));
+            itemDetails.set("description", root.get("description"));
+            itemDetails.set("currencyId", root.get("currencyId"));
+            itemDetails.set("increment", root.get("increment"));
+            itemDetails.set("reservePrice", root.get("reservePrice"));
+            itemDetails.set("auctionDuration", root.get("auctionDuration"));
+            root.remove("title");
+            root.remove("description");
+            root.remove("currencyId");
+            root.remove("increment");
+            root.remove("reservePrice");
+            root.remove("auctionDuration");
+        }
+        return root;
+    }
+}
