2acfc743bafb1123a84c1eb9d6227ad473ef0bc3
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-01-04 10:47:44 +0100

    Updates to latest API (no service-name required/play must use bindServiceInfo)

--- web-gateway/app/Module.java
+++ web-gateway/app/Module.java
@@ -3,17 +3,19 @@ import com.example.auction.item.api.ItemService;
 import com.example.auction.search.api.SearchService;
 import com.example.auction.user.api.UserService;
 import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.api.ServiceAcl;
 import com.lightbend.lagom.javadsl.api.ServiceInfo;
 import com.lightbend.lagom.javadsl.client.ServiceClientGuiceSupport;
 
 public class Module extends AbstractModule implements ServiceClientGuiceSupport {
     @Override
     protected void configure() {
-        bindInfo(new ServiceInfo("web-gateway"));
+        bindServiceInfo(ServiceInfo.of("web-gateway-module", ServiceAcl.path("(?!/api/).*")));
 
         bindClient(UserService.class);
         bindClient(ItemService.class);
         bindClient(BiddingService.class);
         bindClient(SearchService.class);
     }
+
 }
