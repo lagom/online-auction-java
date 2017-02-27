721fe523edb58b9027695fab910fefaa2e1c7f2e
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-02-27 17:23:05 +0100

    disables ACLs from search-api

--- search-api/src/main/java/com/example/auction/search/api/SearchService.java
+++ search-api/src/main/java/com/example/auction/search/api/SearchService.java
@@ -21,6 +21,6 @@ public interface SearchService extends Service {
                 Service.restCall(Method.GET, "/api/search?pageNo&pageSize", this::search)
         ).withPathParamSerializer(
                 UUID.class, PathParamSerializers.optional("UUID", UUID::fromString, UUID::toString)
-        ).withAutoAcl(true);
+        );
     }
 }
