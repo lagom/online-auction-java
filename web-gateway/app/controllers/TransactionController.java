4b81f9e89c1c8e9ceb72739a44628fd53553696a
Yuliana Apaza <yuliana.apaza94@gmail.com>
2017-06-12 03:15:55 -0500

    Set delivery details to readonly for seller and show transaction details

--- web-gateway/app/controllers/TransactionController.java
+++ web-gateway/app/controllers/TransactionController.java
@@ -4,6 +4,7 @@ import com.example.auction.transaction.api.DeliveryInfo;
 import com.example.auction.transaction.api.TransactionInfo;
 import com.example.auction.transaction.api.TransactionInfoStatus;
 import com.example.auction.transaction.api.TransactionService;
+import com.example.auction.user.api.User;
 import com.example.auction.user.api.UserService;
 import com.lightbend.lagom.javadsl.api.transport.TransportException;
 import play.Configuration;
@@ -37,39 +38,73 @@ public class TransactionController extends AbstractController {
         showInlineInstruction = config.getBoolean("online-auction.instruction.show");
     }
 
+    public CompletionStage<Result> getTransaction(String id) {
+        return requireUser(ctx(), user ->
+                loadNav(user).thenCompose(nav -> {
+                    UUID itemId = UUID.fromString(id);
+                    CompletionStage<TransactionInfo> transactionFuture = transactionService.getTransaction(itemId).handleRequestHeader(authenticate(user)).invoke();
+                    return transactionFuture.handle((transaction, exception) ->{
+                        if(exception == null) {
+                            Optional<User> seller = Optional.empty();
+                            Optional<User> winner = Optional.empty();
+                            for (User u : nav.getUsers()) {
+                                if (transaction.getCreator().equals(u.getId())) {
+                                    seller = Optional.of(u);
+                                }
+                                if (transaction.getWinner().equals(u.getId())) {
+                                    winner = Optional.of(u);
+                                }
+                            }
+                            Currency currency = Currency.valueOf(transaction.getItemData().getCurrencyId());
+                            return ok(views.html.transaction.render(showInlineInstruction, Optional.of(transaction), seller, winner, Optional.of(currency), Optional.empty(), nav));
+                        }
+                        else {
+                            String msg = ((TransportException) exception.getCause()).exceptionMessage().detail();
+                            return ok(views.html.transaction.render(showInlineInstruction, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(msg), nav));
+                        }
+                    });
+                })
+        );
+    }
+
     public CompletionStage<Result> submitDeliveryDetailsForm(String id) {
         return requireUser(ctx(), user ->
             loadNav(user).thenCompose(nav -> {
                 UUID itemId = UUID.fromString(id);
                 CompletionStage<TransactionInfo> transactionFuture = transactionService.getTransaction(itemId).handleRequestHeader(authenticate(user)).invoke();
-                return transactionFuture.thenApply(transaction -> {
-                    DeliveryDetailsForm form = new DeliveryDetailsForm();
-                    Optional<DeliveryInfo> maybeDeliveryInfo = transaction.getDeliveryInfo();
-                    if(maybeDeliveryInfo.isPresent()) {
-                        form.setAddressLine1(maybeDeliveryInfo.get().getAddressLine1());
-                        form.setAddressLine2(maybeDeliveryInfo.get().getAddressLine2());
-                        form.setCity(maybeDeliveryInfo.get().getCity());
-                        form.setState(maybeDeliveryInfo.get().getState());
-                        form.setPostalCode(maybeDeliveryInfo.get().getPostalCode());
-                        form.setCountry(maybeDeliveryInfo.get().getCountry());
+                return transactionFuture.handle((transaction, exception) ->{
+                    if(exception == null) {
+                        DeliveryDetailsForm form = new DeliveryDetailsForm();
+                        Optional<DeliveryInfo> maybeDeliveryInfo = transaction.getDeliveryInfo();
+                        if(maybeDeliveryInfo.isPresent()) {
+                            form.setAddressLine1(maybeDeliveryInfo.get().getAddressLine1());
+                            form.setAddressLine2(maybeDeliveryInfo.get().getAddressLine2());
+                            form.setCity(maybeDeliveryInfo.get().getCity());
+                            form.setState(maybeDeliveryInfo.get().getState());
+                            form.setPostalCode(maybeDeliveryInfo.get().getPostalCode());
+                            form.setCountry(maybeDeliveryInfo.get().getCountry());
+                        }
+                        return ok(
+                                views.html.deliveryDetails.render(
+                                        showInlineInstruction,
+                                        !transaction.getCreator().equals(user),
+                                        itemId,
+                                        formFactory.form(DeliveryDetailsForm.class).fill(form),
+                                        transaction.getStatus(),
+                                        Optional.empty(),
+                                        nav)
+                        );
                     }
-
-                    return ok(
-                            views.html.deliveryDetails.render(
-                                    showInlineInstruction,
-                                    itemId,
-                                    formFactory.form(DeliveryDetailsForm.class).fill(form),
-                                    transaction.getStatus(),
-                                    Optional.empty(),
-                                    nav)
-                    );
+                    else {
+                        String msg = ((TransportException) exception.getCause()).exceptionMessage().detail();
+                        return ok(views.html.deliveryDetails.render(showInlineInstruction, false, itemId, formFactory.form(DeliveryDetailsForm.class), TransactionInfoStatus.NEGOTIATING_DELIVERY, Optional.of(msg), nav));
+                    }
                 });
-
             })
         );
     }
 
-    public CompletionStage<Result> submitDeliveryDetails(String id, String transactionStatus) {
+    public CompletionStage<Result> submitDeliveryDetails(String id, String transactionStatus, boolean isBuyer) {
         Http.Context ctx = ctx();
         return requireUser(ctx(), user -> {
 
@@ -79,7 +114,7 @@ public class TransactionController extends AbstractController {
 
             if (form.hasErrors()) {
                 return loadNav(user).thenApply(nav ->
-                        ok(views.html.deliveryDetails.render(showInlineInstruction, itemId, form, status, Optional.empty(), nav))
+                        ok(views.html.deliveryDetails.render(showInlineInstruction, isBuyer, itemId, form, status, Optional.empty(), nav))
                 );
             } else {
                 return transactionService.submitDeliveryDetails(itemId)
@@ -90,9 +125,9 @@ public class TransactionController extends AbstractController {
                                 return CompletableFuture.completedFuture(redirect(controllers.routes.TransactionController.submitDeliveryDetailsForm(id)));
                             } else {
                                 String msg = ((TransportException) exception.getCause()).exceptionMessage().detail();
-                                // TODO: Redirect to show all TransactionData
+                                // TODO: Redirect to show all TransactionInfo
                                 return loadNav(user).thenApply(nav -> ok(
-                                        views.html.deliveryDetails.render(showInlineInstruction, itemId, form, status, Optional.of(msg), nav)));
+                                        views.html.deliveryDetails.render(showInlineInstruction, isBuyer, itemId, form, status, Optional.of(msg), nav)));
                             }
                         }).thenCompose((x) -> x);
             }
