f3f6fcaaac5b7341d132888e7444d6dac376c6c3
lakhina <lakhina.ruchika@gmail.com>
2017-06-21 16:23:13 +0530

    Remove Optional from id of user

--- user-api/src/main/java/com/example/auction/user/api/User.java
+++ user-api/src/main/java/com/example/auction/user/api/User.java
@@ -15,24 +15,10 @@ public final class User {
     private final String email;
 
     @JsonCreator
-    private User(@JsonProperty("id") Optional<UUID> id, @JsonProperty("name") String name, @JsonProperty("email") String email) {
-        this.id = id.orElse(null);
-        this.name = name;
-        this.email = email;
-    }
-
-    public User(UUID id, String name, String email) {
+    public User(@JsonProperty("id") UUID id, @JsonProperty("name") String name, @JsonProperty("email") String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
-    /**
-     * Used when creating a new user.
-     */
-    public User(String name, String email) {
-        this.id = null;
-        this.name = name;
-        this.email = email;
-    }
 }
