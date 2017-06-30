dd97df96595e1501b10288bb16c69f98327b006a
lakhina <lakhina.ruchika@gmail.com>
2017-06-30 20:40:49 +0530

    Update User service impl test

--- user-impl/src/test/java/com/example/auction/user/impl/UserServiceImplTest.java
+++ user-impl/src/test/java/com/example/auction/user/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.auction.user.impl;
 
+import com.example.auction.user.api.User;
 import com.example.auction.user.api.UserRegistration;
 import com.example.auction.user.api.UserService;
 import org.junit.Test;
@@ -9,10 +10,10 @@ import java.util.UUID;
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
 
 public class UserServiceImplTest {
-
-    private final UUID id1= UUID.randomUUID();
+    private final UUID id = UUID.randomUUID();
     private final String name = "admin";
     private final String email = "admin@gmail.com";
     private final String password = "admin";
@@ -22,12 +23,13 @@ public class UserServiceImplTest {
     public void shouldBeAbleToCreateUsers() throws Exception {
         withServer(defaultSetup().withCassandra(true), server -> {
             UserService userService = server.client(UserService.class);
-            UserRegistration userRegistration= new UserRegistration(name,email,password);
-           userService.createUser().invoke(userRegistration).toCompletableFuture().get(10, SECONDS);
-
-
+            User user = new User(id, name, email);
+            UserRegistration userRegistration = new UserRegistration(name, email, password);
+            User user1 = userService.createUser().invoke(userRegistration).toCompletableFuture().get(10, SECONDS);
+            assertEquals(user.getName(), user1.getName());
+            assertEquals(user.getEmail(), user1.getEmail());
+            userService.getUser(user1.getId()).invoke().toCompletableFuture().get(10, SECONDS);
         });
     }
-
 
 }
