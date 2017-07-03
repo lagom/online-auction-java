4a1a62e67d4abe47ba14a3841aef340f96a9544b
lakhina <lakhina.ruchika@gmail.com>
2017-07-03 13:15:58 +0530

    Update UserServiceImplTest

--- user-impl/src/test/java/com/example/auction/user/impl/UserServiceImplTest.java
+++ user-impl/src/test/java/com/example/auction/user/impl/UserServiceImplTest.java
@@ -5,15 +5,13 @@ import com.example.auction.user.api.UserRegistration;
 import com.example.auction.user.api.UserService;
 import org.junit.Test;
 
-import java.util.UUID;
-
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
 public class UserServiceImplTest {
-    private final UUID id = UUID.randomUUID();
+
     private final String name = "admin";
     private final String email = "admin@gmail.com";
     private final String password = "admin";
@@ -23,12 +21,11 @@ public class UserServiceImplTest {
     public void shouldBeAbleToCreateUsers() throws Exception {
         withServer(defaultSetup().withCassandra(true), server -> {
             UserService userService = server.client(UserService.class);
-            User user = new User(id, name, email);
             UserRegistration userRegistration = new UserRegistration(name, email, password);
-            User user1 = userService.createUser().invoke(userRegistration).toCompletableFuture().get(10, SECONDS);
-            assertEquals(user.getName(), user1.getName());
-            assertEquals(user.getEmail(), user1.getEmail());
-            userService.getUser(user1.getId()).invoke().toCompletableFuture().get(10, SECONDS);
+            User createdUser = userService.createUser().invoke(userRegistration).toCompletableFuture().get(10, SECONDS);
+            assertEquals(name, createdUser.getName());
+            assertEquals(email, createdUser.getEmail());
+            assertEquals(createdUser, userService.getUser(createdUser.getId()).invoke().toCompletableFuture().get(10, SECONDS));
         });
     }
 
