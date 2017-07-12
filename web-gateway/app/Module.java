92411dceddc45ed4daaf6b2906b62a4cf7d951e4
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-07-12 08:15:30 +0200

    Fix startup issue caused by removal of play.api.Configuration (#126)

--- web-gateway/app/Module.java
+++ web-gateway/app/Module.java
@@ -7,11 +7,14 @@ import com.lightbend.lagom.javadsl.api.ServiceAcl;
 import com.lightbend.lagom.javadsl.api.ServiceInfo;
 import com.google.inject.AbstractModule;
 import com.lightbend.lagom.javadsl.client.ServiceClientGuiceSupport;
+import com.typesafe.config.Config;
 
 public class Module extends AbstractModule implements ServiceClientGuiceSupport {
     @Override
     protected void configure() {
         bindServiceInfo(ServiceInfo.of("web-gateway-module", ServiceAcl.path("(?!/api/).*")));
+        // TODO: remove this and the ConfigProvider when upgrading to Lagom 1.4.0
+        binder().bind(Config.class).toProvider(ConfigProvider.class);
         bindClient(UserService.class);
         bindClient(ItemService.class);
         bindClient(BiddingService.class);
