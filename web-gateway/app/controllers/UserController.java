85d76dc875861bd56653d22b930b885eb7f6f159
lakhina <lakhina.ruchika@gmail.com>
2017-07-13 17:10:26 +0530

    Update UserController

--- web-gateway/app/controllers/UserController.java
+++ web-gateway/app/controllers/UserController.java
@@ -6,6 +6,7 @@ import play.Configuration;
 import play.data.Form;
 import play.data.FormFactory;
 import play.i18n.MessagesApi;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Http;
 import play.mvc.Result;
 
@@ -17,30 +18,30 @@ public class UserController extends AbstractController {
 
     private final UserService userService;
     private final FormFactory formFactory;
-
+    private HttpExecutionContext httpExecutionContext;
     private final Boolean showInlineInstruction;
 
     @Inject
-    public UserController(Configuration config, MessagesApi messagesApi, UserService userService, FormFactory formFactory) {
+    public UserController(Configuration config, MessagesApi messagesApi, UserService userService, FormFactory formFactory, HttpExecutionContext httpExecutionContext) {
         super(messagesApi, userService);
         this.userService = userService;
         this.formFactory = formFactory;
-
+        this.httpExecutionContext = httpExecutionContext;
         showInlineInstruction = config.getBoolean("online-auction.instruction.show");
     }
 
     public CompletionStage<Result> createUserForm() {
         return withUser(ctx(), userId ->
-                loadNav(userId).thenApply(nav ->
+                loadNav(userId).thenApplyAsync(nav ->
                         ok(views.html.createUser.render(showInlineInstruction, formFactory.form(CreateUserForm.class), nav))
-                )
+                         ,httpExecutionContext.current())
         );
     }
 
     public CompletionStage<Result> createUser() {
         Http.Context ctx = ctx();
         return withUser(ctx, userId ->
-                loadNav(userId).thenCompose(nav -> {
+                loadNav(userId).thenComposeAsync(nav -> {
                     Form<CreateUserForm> form = formFactory.form(CreateUserForm.class).bindFromRequest(ctx.request());
                     if (form.hasErrors()) {
                         return CompletableFuture.completedFuture(ok(views.html.createUser.render(showInlineInstruction, form, nav)));
@@ -52,7 +53,7 @@ public class UserController extends AbstractController {
                                 ctx.session().put("user", user.getId().toString());
                                 return redirect(ProfileController.defaultProfilePage());
                             });
-                })
+                }, httpExecutionContext.current())
         );
     }
 
