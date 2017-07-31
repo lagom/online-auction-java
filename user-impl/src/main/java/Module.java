476e3be8e30a54609aaab189baa1adf86bb57a82
Renato  Cavalcanti <renato@strongtyped.io>
2017-07-31 17:10:33 +0200

    moves Guice module to default package

--- user-impl/src/main/java/Module.java
+++ user-impl/src/main/java/Module.java
@@ -1,12 +1,13 @@
-package com.example.auction.user.impl;
-
 import com.example.auction.user.api.UserService;
+import com.example.auction.user.impl.UserRepository;
+import com.example.auction.user.impl.UserServiceImpl;
 import com.google.inject.AbstractModule;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
 
-public class UserModule extends AbstractModule implements ServiceGuiceSupport {
+public class Module extends AbstractModule implements ServiceGuiceSupport {
     @Override
     protected void configure() {
         bindService(UserService.class, UserServiceImpl.class);
+        bind(UserRepository.class);
     }
 }
