eec950fb232edf490330d5c22911f717bb79747a
Yuliana Apaza <yuliana.apaza94@gmail.com>
2017-08-09 17:47:14 -0500

    Merge with master

--- item-api/src/main/java/com/example/auction/item/api/Location.java
+++ item-api/src/main/java/com/example/auction/item/api/Location.java
@@ -1,9 +1,8 @@
 package com.example.auction.item.api;
 
-import java.util.Optional;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Value;
+
+import java.util.Optional;
 
 @Value
 public final class Location {
@@ -12,7 +11,6 @@ public final class Location {
     private final Optional<String> state;
     private final Optional<String> city;
 
-    @JsonCreator
     public Location(Optional<String> country, Optional<String> state, Optional<String> city) {
         this.country = country;
         this.state = state;
