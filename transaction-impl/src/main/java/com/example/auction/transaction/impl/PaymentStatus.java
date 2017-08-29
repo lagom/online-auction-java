d3fb686ca639a4f47cad2829f3be4dc6836f89d0
Yuliana Apaza <yuliana.apaza94@gmail.com>
2017-08-28 21:13:00 -0500

    Remove comments

--- transaction-impl/src/main/java/com/example/auction/transaction/impl/PaymentStatus.java
+++ transaction-impl/src/main/java/com/example/auction/transaction/impl/PaymentStatus.java
@@ -1,26 +1,6 @@
 package com.example.auction.transaction.impl;
 
-import com.example.auction.transaction.api.PaymentInfoStatus;
-
 public enum PaymentStatus {
-    APPROVED(PaymentInfoStatus.APPROVED), //{
-//        @Override
-        //PaymentInfoStatus toPaymentInfoStatus() {
-  //          return PaymentInfoStatus.APPROVED;
-//        }
-//    },
-    REJECTED(PaymentInfoStatus.REJECTED); //{
-        //@Override
-        //PaymentInfoStatus toPaymentInfoStatus() {
-//            return PaymentInfoStatus.REJECTED;
-//        }
-//    };
-
-    public final PaymentInfoStatus paymentInfoStatus;
-
-    PaymentStatus(PaymentInfoStatus paymentInfoStatus) {
-        this.paymentInfoStatus = paymentInfoStatus;
-    }
-
-    //abstract PaymentInfoStatus toPaymentInfoStatus();
+    APPROVED,
+    REJECTED
 }
